de95f79bbf7d4d1730ef7650bb04ce1bc23d0009
Bratu Sebastian <artweb11@gmail.com>
2023-07-08 15:35:49 +0300

    initial commit, portal

---
+++ packages/portal/src/App.tsx
@@ -0,0 +1,15 @@
+
+function App() {
+  return (
+    <>
+      <div className="w-full h-full flex justify-center items-center">
+        <h2>Welcome to immo</h2>
+      </div>
+      <div className="w-full h-full flex justify-center items-center bg-blue-200">
+        <h2>Welcome to immo, again</h2>
+      </div>
+    </>
+  )
+}
+
+export default App
