5fe08c59476af97e238755e9a165a0dd9ae9ca54
Bratu Sebastian <artweb11@gmail.com>
2023-07-08 19:38:30 +0300

    final release

--- packages/portal/src/App.tsx
+++ packages/portal/src/App.tsx
@@ -3,7 +3,7 @@ function App() {
   return (
     <>
       <div className="w-full h-full flex justify-center items-center">
-        <h2 className="text-xl">Welcome to immobilier</h2>
+        <h2 className="text-xl">Welcome to immo</h2>
       </div>
       <div className="w-full h-full flex justify-center items-center bg-blue-200">
         <h2>Welcome to immo, again</h2>
