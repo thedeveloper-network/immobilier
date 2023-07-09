0636f8b2e690a10307d34909cbb3843fd7bf9939
Bratu Sebastian <artweb11@gmail.com>
2023-07-09 19:44:22 +0300

    worker implem

---
+++ packages/op-worker/index.ts
@@ -0,0 +1,20 @@
+import * as dotenv from 'dotenv';
+dotenv.config();
+
+import { Queue, Worker } from 'bullmq';
+
+const worker = new Worker('immo:work', async (job)=>{
+    await new Promise( ( res ) => {
+        setTimeout( () => res(true), 2000 );
+    });
+
+}, { connection: {
+    host: process.env.REDIS_HOST,
+    port: Number( process.env.REDIS_PORT ),
+    username: process.env.REDIS_USER,
+    password: process.env.REDIS_PASS
+}});
+
+worker.on('completed', job => {
+    console.log(`${job.id} has completed!`);
+});
