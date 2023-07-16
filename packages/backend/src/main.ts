656ef07c583c5b44d235462b292d5803fadc39c0
Bratu Sebastian <artweb11@gmail.com>
2023-07-16 20:20:24 +0300

    fly configuration

--- packages/backend/src/main.ts
+++ packages/backend/src/main.ts
@@ -1,7 +1,7 @@
 import express from 'express';
 
-const host = process.env.HOST ?? 'localhost';
-const port = process.env.PORT ? Number(process.env.PORT) : 3000;
+const host = process.env.HOST ?? '0.0.0.0';
+const port = process.env.PORT ? Number(process.env.PORT) : 8080;
 
 const app = express();
 
