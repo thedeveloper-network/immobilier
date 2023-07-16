fdc074a90a00215fb33b0c0e56d3c2c9313c65be
Bratu Sebastian <artweb11@gmail.com>
2023-07-16 22:57:49 +0300

    op-worker

--- packages/op-worker/src/index.ts
+++ packages/op-worker/src/index.ts
@@ -6,3 +6,12 @@ const a:Listing = {
     name: 'da',
     description: 'sdad'
 }
+
+let count = 0;
+
+const run = () => {
+    console.log('working ', count++ );
+    setTimeout( run, 5000 );
+}
+
+run();
